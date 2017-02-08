7f54f469c372433b67e7777caed6fa9084da77fb
Mario de Frutos <mario.defrutos@cartodb.com>
2017-02-08 11:03:19 +0100

    Include 2.15.x npm version in the version check

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@
  var jasmineCfg = require('./lib/build/tasks/jasmine.js');
  var duplicatedDependencies = require('./lib/build/tasks/shrinkwrap-duplicated-dependencies.js');
 
- var REQUIRED_NPM_VERSION = /2.14.[0-9]+/;
+ var REQUIRED_NPM_VERSION = /2.1[4-5].[0-9]+/;
  var REQUIRED_NODE_VERSION = /0.10.[0-9]+/;
  var SHRINKWRAP_MODULES_TO_VALIDATE = [
   'backbone',
